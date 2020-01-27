e9b1957778170f1991d242318b363b8fa3c9eb01
Abdul <abdul@uitoux.in>
2020-01-27 11:44:25 +0530

    u

--- src/LocationPkgServiceProvider.php
+++ src/LocationPkgServiceProvider.php
@@ -17,6 +17,8 @@ class LocationPkgServiceProvider extends ServiceProvider {
 		$this->loadViewsFrom(__DIR__ . '/views', 'location-pkg');
 		$this->publishes([
 			__DIR__ . '/public' => base_path('public'),
+			__DIR__ . '/database/seeds/client' => 'database/seeds',
+			__DIR__ . '/config/config.php' => config_path('location-pkg.php'),
 		]);
 	}
 
